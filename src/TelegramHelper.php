f9d125e991af66767f734ee1f6b7f359d81a9c15
Luca Patera <lucapatera@outlook.it>
2017-08-19 19:40:23 +0200

    Fixed curl_file_create_auto_mime function

--- src/TelegramHelper.php
+++ src/TelegramHelper.php
@@ -1,17 +1,9 @@
 <?php
 
-/** Helper for Uploading file using CURL */
-if (!function_exists('curl_file_create')) {
-	function curl_file_create($filename, $mimetype = '', $postname = '')
-	{
-		return "@$filename;filename=" . ($postname ?: basename($filename)) . ($mimetype ? ";type=$mimetype" : '');
-	}
-}
-
-/** Helper for Uploading file using CURL with auto mime
+/** Curl file create with auto mime
  * @param string $path Full path file
  * @param string $postname
- * @return string
+ * @return CURLFile
  */
 function curl_file_create_auto_mime($path, $postname = '')
 {
