78158013db3019c817d9997f7b5abf7f0eca75ab
Luca Patera <lucapatera@outlook.it>
2020-04-26 01:49:28 +0200

    Fixed `mb_str_split` method warning in PHP 7.4

--- src/TelegramHelper.php
+++ src/TelegramHelper.php
@@ -1,46 +1,52 @@
 <?php
 
-/**
- * Check if a string is a json
- * @param string $string
- * @return bool
- */
-function is_json($string)
-{
-    return (is_string($string) && is_array(json_decode($string, true)) && (json_last_error() === JSON_ERROR_NONE));
-}
-
-/**
- * Curl file create with auto mime
- * @param string $path Full path file
- * @param string $postname
- * @return CURLFile
- */
-function curl_file_create_automime($path, $postname = '')
-{
-    return curl_file_create($path, mime_content_type($path), $postname);
-}
-
-/**
- * Split an unicode string
- * @param $string
- * @param int $split_length
- * @return array|array[]|bool|false|string[]
- */
-function mb_str_split($string, $split_length = 1)
-{
-    if ($split_length === 1) {
-        return preg_split('//u', $string, -1, PREG_SPLIT_NO_EMPTY);
+if (!function_exists('is_json')) {
+    /**
+     * Check if a string is a json
+     * @param string $string
+     * @return bool
+     */
+    function is_json($string)
+    {
+        return (is_string($string) && is_array(json_decode($string, true)) && (json_last_error() === JSON_ERROR_NONE));
     }
-    
-    if ($split_length > 1) {
-        $return_value = [];
-        $string_length = mb_strlen($string, 'UTF-8');
-        for ($i = 0; $i < $string_length; $i += $split_length) {
-            $return_value[] = mb_substr($string, $i, $split_length, 'UTF-8');
+}
+
+if (!function_exists('curl_file_create_automime')) {
+    /**
+     * Curl file create with auto mime
+     * @param string $path Full path file
+     * @param string $postname
+     * @return CURLFile
+     */
+    function curl_file_create_automime($path, $postname = '')
+    {
+        return curl_file_create($path, mime_content_type($path), $postname);
+    }
+}
+
+if (!function_exists('mb_str_split')) {
+    /**
+     * Split an unicode string
+     * @param $string
+     * @param int $split_length
+     * @return array|array[]|bool|false|string[]
+     */
+    function mb_str_split($string, $split_length = 1)
+    {
+        if ($split_length === 1) {
+            return preg_split('//u', $string, -1, PREG_SPLIT_NO_EMPTY);
         }
-        return $return_value;
+        
+        if ($split_length > 1) {
+            $return_value = [];
+            $string_length = mb_strlen($string, 'UTF-8');
+            for ($i = 0; $i < $string_length; $i += $split_length) {
+                $return_value[] = mb_substr($string, $i, $split_length, 'UTF-8');
+            }
+            return $return_value;
+        }
+        
+        return false;
     }
-    
-    return false;
 }
