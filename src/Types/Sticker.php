472de1308d08bf74eccd797e1cfb4febcb0f48fe
Luca Patera <lucapatera@outlook.it>
2022-04-16 15:34:55 +0200

    Update to Telegram Bot API 5.7

--- src/Types/Sticker.php
+++ src/Types/Sticker.php
@@ -13,55 +13,61 @@ class Sticker
      * @var string $file_id
      */
     public $file_id;
-    
+
     /**
      * Unique identifier for this file, which is supposed to be the same over time and for different bots. Can't be used to download or reuse the file.
      * @var string $file_unique_id
      */
     public $file_unique_id;
-    
+
     /**
      * Sticker width
      * @var int $width
      */
     public $width;
-    
+
     /**
      * Sticker height
      * @var int $height
      */
     public $height;
-    
+
     /**
      * True, if the sticker is {@see https://telegram.org/blog/animated-stickers animated}
      * @var bool $is_animated
      */
     public $is_animated;
-    
+
     /**
-     * Optional. Sticker thumbnail in .webp or .jpg format
+     * True, if the sticker is a {@see https://telegram.org/blog/video-stickers-better-reactions video sticker}
+     * @var bool $is_video
+     */
+    public $is_video;
+
+    /**
+     * Optional. Sticker thumbnail in the .WEBP or .JPG format
      * @var PhotoSize $thumb
      */
     public $thumb;
-    
+
     /**
      * Optional. Emoji associated with the sticker
      * @var string $emoji
      */
     public $emoji;
-    
+
     /**
      * Optional. Name of the sticker set to which the sticker belongs
      * @var string $set_name
      */
     public $set_name;
-    
+
     /**
      * Optional. For mask stickers, the position where the mask should be placed
      * @var MaskPosition $mask_position
      */
     public $mask_position;
-    
+
     /**
      * Optional. File size
      * @var int $file_size
--- src/Types/StickerSet.php
+++ src/Types/StickerSet.php
@@ -13,33 +13,39 @@ class StickerSet
      * @var string $name
      */
     public $name;
-    
+
     /**
      * Sticker set title
      * @var string $title
      */
     public $title;
-    
+
     /**
      * True, if the sticker is animated
      * @var bool $is_animated
      */
     public $is_animated;
-    
+
+    /**
+     * True, if the sticker set contains {@see https://telegram.org/blog/video-stickers-better-reactions video stickers}
+     * @var bool $is_video
+     */
+    public $is_video;
+
     /**
      * True, if the sticker set contains masks
      * @var bool $contains_masks
      */
     public $contains_masks;
-    
+
     /**
      * List of all set stickers
      * @var Sticker[] $stickers
      */
     public $stickers;
-    
+
     /**
-     * Optional. Sticker set thumbnail in the .WEBP or .TGS format
+     * Optional. Sticker set thumbnail in the .WEBP or .TGS, or .WEBM format
      * @var PhotoSize $thumb
      */
     public $thumb;
